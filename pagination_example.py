c03f6a8fac65525fb73b1cadbfc285cfed2f25ee
Ben Elsworth <be15516@ieu-db-interface.epi.bris.ac.uk>
2021-03-29 11:00:24 +0100

    multi search

--- pagination_example.py
+++ pagination_example.py
@@ -1,6 +1,8 @@
 import argparse
 import os
+import time
 from elasticsearch import Elasticsearch
+from elasticsearch import helpers
 
 parser = argparse.ArgumentParser(description='Range query.')
 parser.add_argument('-e,--ehost', dest='ehost', help='elasticsearch host', default='localhost')
@@ -34,9 +36,56 @@ def get_test_snps():
 
 def read_snps():
     with open(snp_list_file) as f:
-        lines = f.read().splitlines()
-        print(lines[:10])
+        snps = f.read().splitlines()
+        print(snps[:10])
+        return snps
 
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+def make_multi_body_text(filterData):
+    m = {
+        "size":10000,
+        "query": {
+            "bool" : {
+                "filter" : filterData
+                }
+            }
+        }
+    return m
+
+def elastic_search_multi(bodyText):
+    res = es.msearch(
+        body=bodyText,request_timeout=300)
+    return res
+
+def multi(snp_list,index_name,gwas_list):
+    res = []
+    request = []
+    for c in chunks(snp_list,10000):
+        print(len(c))
+        req_head = {'index': index_name, "timeout":300, "ignore_unavailable":True}
+        filterData = []
+        filterData.append({"terms": {'snp_id': c}})
+        filterData.append({"terms": {'gwas_id': gwas_list}})
+        bodyText=make_multi_body_text(filterData)
+        request.extend([req_head, bodyText])
+    #print(request)
+    start = time.time()
+    e = elastic_search_multi(request)
+    for response in e['responses']:
+        for r in response['hits']['hits']:
+            #print(r['_source'])
+            res.append(r['_source'])
+    end = time.time()
+    t = round((end - start), 4)
+    print("Time taken: " + str(t) + " seconds")
+    print('ES returned ' + str(len(res)) + ' records')
+    #print(res)
 
 get_test_snps()
-read_snps()
+snp_list = read_snps()
+#scan(snp_list)
+multi(snp_list,'ieu-b',gwas_list=['1','2'])
